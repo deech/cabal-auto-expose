e4520e34db9724d40ae8aa2c743a34d02841123a
deech <aditya.siram@gmail.com>
2020-09-07 13:43:28 -0500

    Add some API docs.

--- cabal-auto-expose/src/Distribution/Simple/AutoExpose.hs
+++ cabal-auto-expose/src/Distribution/Simple/AutoExpose.hs
@@ -1,14 +1,15 @@
+-- | Import this module in your @Setup.hs@ to auto detect library modules in
+-- your project The API does not conceal it's internals but in most cases you
+-- should only need 'defaultMain' or 'defaultMainWithHooks'.
 module Distribution.Simple.AutoExpose where
 
 import Control.Exception(catch,IOException)
-import Control.Monad((>=>),filterM,forM)
+import Control.Monad((>=>),filterM)
 import Control.Monad.Extra(ifM,notM)
 import Data.List(intercalate,nub)
-import Data.Maybe(maybeToList)
-import Distribution.Compat.Lens((%~),(&))
+import Distribution.Compat.Lens((%~))
 import Distribution.ModuleName(ModuleName,fromString,validModuleComponent)
 import Distribution.PackageDescription(hsSourceDirs,buildInfo,testBuildInfo,benchmarkBuildInfo,executables,testSuites,benchmarks,libBuildInfo,subLibraries,library,Library)
-import Distribution.Simple(defaultMainWithHooks,simpleUserHooks)
 import Distribution.Simple.BuildPaths(autogenPathsModuleName)
 import Distribution.Simple.PreProcess(knownSuffixHandlers)
 import Distribution.Simple.Setup(BuildFlags, ReplFlags, HscolourFlags, HaddockFlags, CopyFlags, InstallFlags, TestFlags, BenchmarkFlags, RegisterFlags, DoctestFlags)
@@ -19,13 +20,20 @@ import Distribution.Types.PackageDescription(PackageDescription)
 import GHC.Stack(HasCallStack)
 import System.Directory(makeAbsolute,listDirectory,doesDirectoryExist,withCurrentDirectory,pathIsSymbolicLink)
 import System.FilePath(splitDirectories, dropExtension, takeExtension,equalFilePath,makeRelative)
-import qualified Distribution.Types.Library.Lens as L
+import qualified Distribution.Simple(defaultMainWithHooks,simpleUserHooks)
 import qualified Distribution.Types.BuildInfo.Lens as L
+import qualified Distribution.Types.Library.Lens as L
 
+-- | The supported Haskell source extensions, just 'hs' and 'lhs'
 sourceExtensions :: [String]
 sourceExtensions = ["hs","lhs"]
 
-moduleNamesToExpose :: [String] -> [FilePath] -> [ModuleName]
+-- | Search for file paths that look like valid modules and convert to the
+--   components to the Cabal internal 'ModuleName'.
+moduleNamesToExpose
+  :: [String] -- ^ File extensions of valid Haskell modules, includes pre-processor extensions
+  -> [FilePath] -- ^ File paths to search
+  -> [ModuleName]
 moduleNamesToExpose extensions =
   map (fromString . intercalate ".")
   . filter (all validModuleComponent)
@@ -41,7 +49,15 @@ moduleNamesToExpose extensions =
     toModuleComponents =
       splitDirectories . dropExtension
 
-getDirectoryContents :: FilePath -> [FilePath] -> IO [FilePath]
+-- | Recursively collect the files in a directory, optionally excluding some
+-- files. Symlinks are ignored and collected paths are relative to the search
+-- directory, eg. if the search directory is @\/home\/user\/myproject\/src@ the path
+-- @\/home\/user\/myproject\/src\/A\/B\/C.hs@ is returned as @A\/B\/C.hs@ so it can
+-- converted by 'moduleNamesToExpose' to a valid module name.
+getDirectoryContents
+  :: FilePath -- ^ Directory to search (path must be absolute)
+  -> [FilePath] -- ^ Paths to ignore (paths must be absolute)
+  -> IO [FilePath] -- ^ File paths made relative to the search directory
 getDirectoryContents dir excludedDirs = do
   (map (makeRelative dir)) <$> go [dir] []
   where
@@ -59,6 +75,9 @@ getDirectoryContents dir excludedDirs = do
             (go fs (f:accum))
     go [] accum = pure accum
 
+-- | Source directories for all non-library components (benchmarks, executables
+-- etc.) so that we can exclude their modules if they appear inside the library's
+-- source directory
 nonLibraryHsSourcePaths :: PackageDescription -> [[FilePath]]
 nonLibraryHsSourcePaths pd =
   map hsSourceDirs $
@@ -66,6 +85,11 @@ nonLibraryHsSourcePaths pd =
   ++ (map testBuildInfo (testSuites pd))
   ++ (map benchmarkBuildInfo (benchmarks pd))
 
+-- | Associate each item in a list will it's left and right elements, eg.
+-- > indexWithNeighbors [1,2,3,4] == [(1,[2,3,4]),(2,[1,3,4]),(3,[1,2,4]),(4,[1,2,3])]
+--
+-- Used to associate a source directory with possibly inner directories that
+-- should be ignored with searching for Haskell modules.
 indexWithNeighbors :: [a] -> [(a,[a])]
 indexWithNeighbors (a:as) = reverse (go [] a as [])
   where
@@ -74,14 +98,19 @@ indexWithNeighbors (a:as) = reverse (go [] a as [])
     go ls x [] accum = (x,ls):accum
 indexWithNeighbors [] = []
 
-data ExposedLibs =
-  ExposedLibs
-  { exposedLibsMainLib :: [ModuleName]
-  , exposedLibsSubLibs :: [(Library,[ModuleName])]
-  }
+-- | A wrapper around a list of 'ModuleName' for a more precise type
+--
+newtype ExposedLib =
+  ExposedLib { exposedLibMainLib :: [ModuleName] }
   deriving Show
 
-getExposedModules :: [String] -> [FilePath] -> [[FilePath]] -> IO [ModuleName]
+-- | Drill into the source trees for a component and find modules
+-- excluding the source trees for other components
+getExposedModules
+  :: [String] -- ^ Known Haskell and preprocessor extensions
+  -> [FilePath] -- ^ The source trees for this component
+  -> [[FilePath]] -- ^ The source trees for all the other components
+  -> IO [ModuleName] -- ^ Detect modules in this component
 getExposedModules ppExts hsSrcDirs otherHsSrcDirs = do
   absHsSrcDirs <- mapM makeAbsolute hsSrcDirs
   absOtherHsSrcDirs <- mapM makeAbsolute (concat otherHsSrcDirs)
@@ -93,31 +122,27 @@ getExposedModules ppExts hsSrcDirs otherHsSrcDirs = do
         )
   pure $ moduleNamesToExpose (sourceExtensions ++ ppExts) contents
 
-getExposedLibs :: PackageDescription -> UserHooks -> IO ExposedLibs
+-- | Pull out the main library from the 'PackageDescription' of this
+-- project and get a list of detected Haskell modules
+getExposedLibs
+  :: PackageDescription
+  -> UserHooks -- ^ So we can grab the 'hookedPreProcessors' extensions
+  -> IO [ModuleName]
 getExposedLibs pd uhs =
-  pure ExposedLibs
-       <*> exposeMainLib
-       <*> exposeSubLibs
+  let excluded =
+        map libSrcDir (subLibraries pd) ++ (nonLibraryHsSourcePaths pd)
+      libExposedModules l =
+        getExposedModules ppExts (libSrcDir l) excluded
+  in maybe (pure []) libExposedModules (library pd)
   where
     ppExts :: [String]
     ppExts = (nub . map fst) ((hookedPreProcessors uhs) ++ knownSuffixHandlers)
     libSrcDir :: Library -> [FilePath]
     libSrcDir = nub . hsSourceDirs . libBuildInfo
-    exposeMainLib :: IO [ModuleName]
-    exposeMainLib =
-      let excluded =
-            map libSrcDir (subLibraries pd) ++ (nonLibraryHsSourcePaths pd)
-          libExposedModules l =
-            getExposedModules ppExts (libSrcDir l) excluded
-      in maybe (pure []) libExposedModules (library pd)
-    exposeSubLibs :: IO [(Library,[ModuleName])]
-    exposeSubLibs =
-      let excluded =
-            nonLibraryHsSourcePaths pd ++ map libSrcDir (maybeToList (library pd))
-          libExposedModules l ls =
-            getExposedModules ppExts (libSrcDir l) ((map libSrcDir ls) ++ excluded)
-      in forM (indexWithNeighbors (subLibraries pd)) (\(l,ls) -> ((,) l) <$> libExposedModules l ls)
 
+-- | Update the 'PackageDescription' of this package to include auto detected
+-- library modules. Also just to be nice fill in the 'Paths_...' module in
+-- 'otherModules' field of the library's 'BuildInfo'.
 updatePackageDescription :: HasCallStack => PackageDescription -> UserHooks -> IO PackageDescription
 updatePackageDescription pd uhs = do
   cabalFilePath <- findPackageDesc "."
@@ -128,12 +153,12 @@ updatePackageDescription pd uhs = do
         exposedLibs <- getExposedLibs pd uhs
         let newMainLibrary =
              (L.libBuildInfo . L.otherModules %~ (nub . (++) [(autogenPathsModuleName pd)]))
-             . (L.exposedModules %~ (nub . (++) (exposedLibsMainLib exposedLibs)))
+             . (L.exposedModules %~ (nub . (++) exposedLibs))
         pure $
-          pd { library = fmap newMainLibrary (library pd)
-             , subLibraries = map (\(l,eps) -> l & L.exposedModules %~ (nub . (++) eps)) (exposedLibsSubLibs exposedLibs)
-             }
+          pd { library = fmap newMainLibrary (library pd) }
 
+-- | Modify a set of 'UserHooks' so that all relevant hooks see a 'PackageDescription' with
+-- auto detected modules filled in.
 autoExposeHooks :: UserHooks -> UserHooks
 autoExposeHooks userHooks =
   userHooks
@@ -195,5 +220,16 @@ autoExposeHooks userHooks =
       newPd <- updatePackageDescription pd uhs
       (unregHook userHooks) newPd lbi uhs fs
 
+-- | If you have already using custom 'UserHooks' use this in your Setup.hs's 'main'
+--
+-- > import qualified Distribution.Simple.AutoExpose as AutoExpose
+-- > main = AutoExpose.defaultMainWithHooks myHooks
+defaultMainWithHooks :: UserHooks -> IO ()
+defaultMainWithHooks uhs = Distribution.Simple.defaultMainWithHooks (autoExposeHooks uhs)
+
+-- | The common case top level function where this library is the only custom part of your project
+--
+-- > import qualified Distribution.Simple.AutoExpose
+-- > main = AutoExpose.defaultMain
 defaultMain :: IO ()
-defaultMain = defaultMainWithHooks (autoExposeHooks simpleUserHooks)
+defaultMain = defaultMainWithHooks Distribution.Simple.simpleUserHooks
